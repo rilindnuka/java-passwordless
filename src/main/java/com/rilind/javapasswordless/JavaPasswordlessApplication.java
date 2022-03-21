f5acc06c0de7789dda03e24f02c28bbcdbe4bb2a
Rilind Nuka <rilindnuka@gmail.com>
2022-03-21 15:47:44 +0100

    Initial project initialization

---
+++ src/main/java/com/rilind/javapasswordless/JavaPasswordlessApplication.java
@@ -0,0 +1,13 @@
+package com.rilind.javapasswordless;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPasswordlessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaPasswordlessApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rilind/javapasswordless/JavaPasswordlessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rilind.javapasswordless;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPasswordlessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
